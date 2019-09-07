1a4a058af2e90fb676444067bcacd6f553326f79
Nicholas Corgan <n.corgan@gmail.com>
2019-09-07 12:56:48 -0500

    Added Label

---
+++ java/Label.java
@@ -0,0 +1,90 @@
+// Copyright (c) 2019 Nicholas Corgan
+// SPDX-License-Identifier: BSL-1.0
+
+package Pothos;
+
+public class Label
+{
+    private Proxy labelProxy;
+
+    public Label(String id, Object data, long index)
+    {
+        this(id, data, index, 1);
+    }
+
+    public Label(String id, Object data, long index, long width)
+    {
+        ProxyEnvironment proxyEnv = new ProxyEnvironment("managed");
+        Proxy labelClassProxy = proxyEnv.findProxy("Pothos/Label");
+
+        Object[] args = new Object[4];
+        args[0] = id;
+        args[1] = data;
+        args[2] = index;
+        args[3] = width;
+
+        labelProxy = labelClassProxy.call("()", args);
+    }
+
+    public Label(Proxy proxy)
+    {
+        labelProxy = proxy;
+    }
+
+    public Label toAdjusted(long mult, long div)
+    {
+        Object[] args = new Object[2];
+        args[0] = mult;
+        args[1] = div;
+
+        return new Label(labelProxy.call("()", args));
+    }
+
+    public Label adjust(long mult, long div)
+    {
+        Object[] args = new Object[2];
+        args[0] = mult;
+        args[1] = div;
+
+        return new Label(labelProxy.call("adjust", args));
+    }
+
+    public String id()
+    {
+        return (String)managedGet("id");
+    }
+
+    public Object data()
+    {
+        return managedGet("data");
+    }
+
+    public long index()
+    {
+        return (long)managedGet("index");
+    }
+
+    public long width()
+    {
+        return (long)managedGet("width");
+    }
+
+    //
+    // Helpers
+    //
+
+    private Object managedCall(String name)
+    {
+        return labelProxy.call(name).toObject();
+    }
+
+    private Object managedCall(String name, Object[] args)
+    {
+        return labelProxy.call(name, args).toObject();
+    }
+
+    private Object managedGet(String field)
+    {
+        return labelProxy.get(field).toObject();
+    }
+}
